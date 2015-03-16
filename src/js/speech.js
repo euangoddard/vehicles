6eb636b45b233e00bed3fc61658b12c84c386ad7
Euan Goddard <euangoddard@2degreesnetwork.com>
2015-03-16 13:21:03 +0000

    Implemented basic service for speech

--- src/js/speech.js
+++ src/js/speech.js
@@ -1,8 +1,34 @@
 var speech = angular.module('speech', []);
 
-speech.provider('Speech', function () {
+speech.factory('Speech', function ($log) {
 
-	this.$get = function () {
-		return {};
-	};
+    var is_available = ('speechSynthesis' in window);
+
+    var get_voice_for_language = function (language) {
+        var voices = window.speechSynthesis.getVoices();
+        var preferred_voice = null;
+        angular.forEach(voices, function (voice) {
+            if (language === voice.lang) {
+                preferred_voice = voice;
+            }
+        });
+
+        if (!preferred_voice) {
+            $log.warn('Could not select voice for "%s"', language);
+        }
+        return preferred_voice;
+    };
+
+    var service = {
+        say: function (text, language) {
+            var utterance = new SpeechSynthesisUtterance(text);
+            var voice = get_voice_for_language(language);
+            if (voice) {
+                utterance.voice = voice;
+            }
+            window.speechSynthesis.speak(utterance);
+        }
+
+    };
+    return service;
 });
--- src/js/vehicles.js
+++ src/js/vehicles.js
@@ -1,6 +1,9 @@
-var vehicles = angular.module('vehicles', []);
+var vehicles = angular.module('vehicles', ['speech']);
 
 
-vehicles.controller('test', function ($scope) {
-	$scope.test = 'It is a test';
+vehicles.controller('test', function ($scope, Speech) {
+    $scope.speech = {text: 'This is a test'};
+    $scope.speak = function () {
+        Speech.say($scope.speech.text, 'en-GB');
+    };
 });
