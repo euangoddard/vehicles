6d417506e164a099ae740764fbcd8c7f6b9a9ebb
Euan Goddard <euangoddard@2degreesnetwork.com>
2015-03-18 12:44:38 +0000

    Added promise to speaking service

--- src/js/speech.js
+++ src/js/speech.js
@@ -1,6 +1,6 @@
 var speech = angular.module('speech', []);
 
-speech.factory('Speech', function ($log) {
+speech.factory('Speech', function ($log, $q) {
 
     var is_available = ('speechSynthesis' in window);
 
@@ -10,12 +10,9 @@ speech.factory('Speech', function ($log) {
     };
 
     var get_voice_for_language = function (language) {
-        var preferred_voice = null;
-        angular.forEach(voices, function (voice) {
-            if (language === voice.lang) {
-                preferred_voice = voice;
-            }
-        });
+        var voice_matches_language = function (v) { return v.lang === language};
+        var voices_for_lang = voices.filter(voice_matches_language);
+        var preferred_voice = voices_for_lang.slice(-1);
 
         if (!preferred_voice) {
             $log.warn('Could not select voice for "%s"', language);
@@ -26,13 +23,18 @@ speech.factory('Speech', function ($log) {
     var service = {
         say: function (text, language) {
             var utterance = new SpeechSynthesisUtterance(text);
+            var utterance_promise = $q(function (resolve, reject) {
+                utterance.onend = resolve;
+                utterance.onerror = reject;
+            });
             var voice = get_voice_for_language(language);
             if (voice) {
                 utterance.voice = voice;
             } else {
                 utterance.lang = language;
             }
-            window.speechSynthesis.speak(utterance);
+            speechSynthesis.speak(utterance);
+            return utterance_promise;
         }
 
     };
--- src/js/vehicles.js
+++ src/js/vehicles.js
@@ -3,7 +3,11 @@ var vehicles = angular.module('vehicles', ['speech']);
 
 vehicles.controller('test', function ($scope, Speech) {
     $scope.speech = {text: 'This is a test'};
+    $scope.is_speaking = false;
     $scope.speak = function () {
-        Speech.say($scope.speech.text, 'en-GB');
+        $scope.is_speaking = true;
+        Speech.say($scope.speech.text, 'en-GB').then(function () {
+            $scope.is_speaking = false;
+        });
     };
 });
